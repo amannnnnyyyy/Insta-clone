47014d141ae6dbcdab4e1eb01de0353012834a1d
Amanuel Moha <amanuelmoha.official@gmail.com>
2023-12-08 13:41:25 +0300

    New additions Auth form

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,23 @@
 import { Box, Button,Flex, Image, Input,Text, VStack } from '@chakra-ui/react'
 import { useState } from 'react'
+import { useNavigate, useNavigation } from 'react-router-dom';
 const AuthForm = () => {
-    const [isLogin, setIsLogin] = useState(true)
+    const [isLogin, setIsLogin] = useState(true);
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({
+email:'',
+password:'',
+confirmPassword:''
+    });
+    const handleAuth = () => {
+        if(!inputs.email || !inputs.password) {
+            alert('Please enter all fields required');
+            return
+    }
 
-  return <>
+    navigate("/")
+    };
+  return ( <>
   
   <Box border={"1px solid gray"} borderRadius={4} padding={5}>
         <VStack spacing={4}>
@@ -12,22 +26,28 @@ const AuthForm = () => {
             placeholder='Email'
             fontSize={14}
             type='email'
+            value = {inputs.email}
+            onChange={(e) => setInputs({...inputs,email: e.target.value})}
         />
 
 <Input
             placeholder='Password'
             fontSize={14}
             type='password'
+            value = {inputs.password}
+            onChange={(e) => setInputs({...inputs,password: e.target.value})}
         />
     {!isLogin ? (
         <Input
         placeholder='Confirm Password'
+        value = {inputs.confirmPassword}
+            onChange={(e) => setInputs({...inputs,confirmPassword: e.target.value})}
         fontSize={14}
         type='password'
     />
     ) : null}
 
-    <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14}>
+    <Button w={"full"} colorScheme='blue' size={"sm"} fontSize={14} onClick={handleAuth}>
         {isLogin? "Log in" : "Sign Up" }
     </Button>
 
@@ -60,6 +80,6 @@ const AuthForm = () => {
             </Box>  
 
   </>
-}
+)}
 
 export default AuthForm
